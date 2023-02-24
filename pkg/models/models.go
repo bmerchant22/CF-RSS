0a280f66ebaa1bbafde2a8a6aae19376df3306d1
Burhanuddin Merchant <merchantburhanuddin484@gmail.com>
2023-02-24 15:07:06 +0530

    RecentActions struct type made, all models built.

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -1,1 +1,31 @@
 package models
+
+type BlogEntry struct {
+	Id                      int    `json:"id"`
+	OriginalLocale          string `json:"originalLocale"`
+	CreationTimeSeconds     int    `json:"creationTimeSeconds"`
+	AuthorHandle            string `json:"authorHandle"`
+	Title                   string `json:"title"`
+	Content                 string `json:"content"`
+	Locale                  string `json:"locale"`
+	ModificationTimeSeconds int    `json:"modificationTimeSeconds"`
+	AllowViewHistory        bool   `json:"allowViewHistory"`
+	Tags                    string `json:"tags"`
+	Rating                  int    `json:"rating"`
+}
+
+type Comment struct {
+	Id                  int    `json:"id"`
+	CreationTimeSeconds int    `json:"creationTimeSeconds"`
+	CommentatorHandle   string `json:"commentatorHandle"`
+	Locale              string `json:"locale"`
+	Text                string `json:"text"`
+	ParentCommentId     int    `json:"parentCommentId"`
+	Rating              int    `json:"rating"`
+}
+
+type RecentAction struct {
+	TimeSeconds int      `json:"timeSeconds"`
+	BlogEntry   struct{} `json:"blogEntry"`
+	Comment     struct{} `json:"comment"`
+}
