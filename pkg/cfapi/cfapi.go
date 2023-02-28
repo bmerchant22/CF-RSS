5d17ffa1363c5c9d6f7bab9e1a5c5eab401f45cb
Burhanuddin Merchant <merchantburhanuddin484@gmail.com>
2023-02-28 17:24:40 +0530

    created RecentActions method, got data from cfapi, unmarshalled it, returned the result.

--- pkg/cfapi/cfapi.go
+++ pkg/cfapi/cfapi.go
@@ -1,7 +1,49 @@
 package cfapi
 
-import "github.com/bmerchant2253/project/pkg/models"
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/bmerchant22/project/pkg/models"
+	"go.uber.org/zap"
+	"io"
+	"net/http"
+)
 
 type CodeforcesAPI interface {
 	RecentActions(maxCount int) ([]models.RecentAction, error)
+}
+
+type CodeforcesClient struct {
+	client http.Client
+}
+
+func (cfClient *CodeforcesClient) RecentActions(maxCount int) ([]models.RecentAction, error) {
+	resp, err := cfClient.client.Get("https://codeforces.com/api/recentActions?maxCount=30")
+	if err != nil {
+		zap.S().Errorf("Error occured while calling cf api: %v", err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("error occured while reading the resp body")
+		return nil, err
+	}
+
+	//zap.S().Info(string(data))
+
+	wrapper := struct {
+		Status string
+		Result []models.RecentAction
+	}{}
+
+	json.Unmarshal(data, &wrapper)
+
+	return wrapper.Result, err
+}
+
+func NewCodeforcesClient() CodeforcesAPI {
+	obj := new(CodeforcesClient)
+	return obj
 }
