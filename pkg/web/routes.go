105bc4b203da6d1cd3c49c92e6cf30d12034b9fc
Burhanuddin Merchant <merchantburhanuddin484@gmail.com>
2023-03-11 03:11:50 +0530

    Added const for routes

---
+++ pkg/web/routes.go
@@ -0,0 +1,11 @@
+package web
+
+const (
+	kHome                 = "/"
+	kUserSignup           = "/user/signup"
+	kRecentActions        = "/activity/recent-actions"
+	kSubscribeToBlogs     = "/user/blogs/subscribe"
+	kUnsubscribeFromBlogs = "/user/blogs/unsubscribe"
+	kCommentsFromBlog     = "/blogs/:id/comments"
+	kRecentActionsForUser = "/user/activity/recent-actions"
+)
