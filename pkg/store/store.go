9739d15ddbfd55e698e4f55050cb217301aec7ca
Burhanuddin Merchant <merchantburhanuddin484@gmail.com>
2023-03-11 03:15:25 +0530

    Created functions to call at different routes, with filters

--- pkg/store/store.go
+++ pkg/store/store.go
@@ -14,10 +14,10 @@ import (
 const uri = "mongodb://localhost:27017/"
 
 type MongoStore struct {
-	RecentActionCollection *mongo.Collection
+	Collection *mongo.Collection
 }
 
-func (m *MongoStore) ConnectToDatabase() {
+func (m *MongoStore) ConnectToDatabaseRecentAction() {
 
 	// Use the SetServerAPIOptions() method to set the Stable API version to 1
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
@@ -41,9 +41,39 @@ func (m *MongoStore) ConnectToDatabase() {
 		return
 	}
 
-	fmt.Println("Pinged the primary node of the cluster. You successfully connected to MongoDB!")
+	fmt.Println("Pinged the primary node of the cluster. You successfully connected to MongoDB! Collection : recent_actions_demo")
 
-	m.RecentActionCollection = client.Database("config").Collection("recent_actions_demo")
+	m.Collection = client.Database("config").Collection("recent_actions_demo")
+
+}
+
+func (m *MongoStore) ConnectToDatabaseUsers() {
+
+	// Use the SetServerAPIOptions() method to set the Stable API version to 1
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
+
+	// Create a new client and connect to the server
+	client, err := mongo.Connect(context.TODO(), opts)
+
+	if err != nil {
+		panic(err)
+	}
+	/*defer func main() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()*/
+
+	// Send a ping to confirm a successful connection
+	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		panic(err)
+		return
+	}
+
+	fmt.Println("Pinged the primary node of the cluster. You successfully connected to MongoDB! Collection : Users")
+
+	m.Collection = client.Database("config").Collection("users")
 
 }
 
@@ -55,7 +85,7 @@ func (m *MongoStore) StoreRecentActionsInTheDatabase(actions []models.RecentActi
 
 	zap.S().Info("Trying to insert document to mongodb")
 
-	_, err1 := m.RecentActionCollection.InsertMany(context.TODO(), toInsertInterface)
+	_, err1 := m.Collection.InsertMany(context.TODO(), toInsertInterface)
 	if err1 != nil {
 		zap.S().Errorf("Error while inserting documents: %v", err1)
 		return nil
@@ -70,10 +100,10 @@ func (m *MongoStore) GetMaxTimeStamp() (int64, error) {
 	groupStage := bson.D{
 		{"$group", bson.D{
 			{"_id", nil},
-			{"MaxTimeStamp", bson.D{{"$max", "$timeseconds"}}},
+			{"MaxTimeStamp", bson.D{{"$max", "$timeSeconds"}}},
 		}},
 	}
-	cursor, err := m.RecentActionCollection.Aggregate(context.TODO(), mongo.Pipeline{groupStage})
+	cursor, err := m.Collection.Aggregate(context.TODO(), mongo.Pipeline{groupStage})
 	if err != nil {
 		panic(err)
 	}
@@ -89,8 +119,8 @@ func (m *MongoStore) GetMaxTimeStamp() (int64, error) {
 }
 
 func (m *MongoStore) QueryRecentActions(after int64) ([]models.RecentAction, error) {
-	filter := bson.M{"timeseconds": bson.M{"$gte": after}}
-	cursor, err := m.RecentActionCollection.Find(context.TODO(), filter)
+	filter := bson.M{"timeSeconds": bson.M{"$gte": after}}
+	cursor, err := m.Collection.Find(context.TODO(), filter)
 	if err != nil {
 		zap.S().Errorf("Error while quering mongoDB")
 	}
@@ -110,4 +140,101 @@ func (m *MongoStore) QueryRecentActions(after int64) ([]models.RecentAction, err
 		}
 	}
 	return results, err
+}
+
+func (m *MongoStore) UserSignup(User models.User) error {
+	m.ConnectToDatabaseUsers()
+
+	wrapper := models.User{}
+
+	wrapper = User
+
+	var a, b interface{}
+	res := m.Collection.FindOne(context.TODO(), bson.M{"email": wrapper.Email}).Decode(&a)
+	res1 := m.Collection.FindOne(context.TODO(), bson.M{"username": wrapper.Username}).Decode(&b)
+	if res == mongo.ErrNoDocuments && res1 == mongo.ErrNoDocuments {
+		_, err := m.Collection.InsertOne(context.TODO(), User)
+		if err != nil {
+			zap.S().Errorf("Error while storing User details: %v", err)
+			return nil
+		}
+		zap.S().Infof("User signed up successfully")
+		return nil
+	}
+
+	zap.S().Errorf("Bad credentials")
+	return nil
+}
+
+func (m *MongoStore) SubscribeToBlog(username string, BlogID int) error {
+	m.ConnectToDatabaseUsers()
+
+	filter := bson.M{
+		"username":        username,
+		"subscribedBlogs": bson.M{"$not": bson.M{"$all": bson.A{BlogID}}},
+	}
+	update := bson.M{"$push": bson.M{"subscribedBlogs": BlogID}}
+
+	var result interface{}
+	err := m.Collection.FindOneAndUpdate(context.TODO(), filter, update).Decode(&result)
+	if err == mongo.ErrNoDocuments {
+		zap.S().Errorf("Error while adding %v blog to subscribed list of %v user due to %v", BlogID, username, err)
+	}
+
+	zap.S().Infof("Successfully added %v blogID to %v user", BlogID, username)
+	return err
+}
+
+func (m *MongoStore) UnsubscribeFromBlog(username string, BlogID int) error {
+	m.ConnectToDatabaseUsers()
+
+	filter := bson.M{"username": username}
+	update := bson.M{"$pull": bson.M{"subscribedBlogs": BlogID}}
+
+	var result interface{}
+	err := m.Collection.FindOneAndUpdate(context.TODO(), filter, update).Decode(&result)
+	if err == mongo.ErrNoDocuments {
+		zap.S().Errorf("Error while removing %v blog from subscribed list of %v user due to %v", BlogID, username, err)
+	}
+
+	zap.S().Infof("Successfully removed %v blogID from %v user", BlogID, username)
+	return err
+}
+
+func (m *MongoStore) QueryRecentActionsForUser(username string, after int64, limit int64) ([]models.RecentAction, error) {
+	m.ConnectToDatabaseUsers()
+
+	filter := bson.M{"username": username}
+	result := models.User{}
+	if err := m.Collection.FindOne(context.TODO(), filter).Decode(&result); err != nil {
+		zap.S().Errorf("Error while finding the username to query recent actions for user")
+		return nil, nil
+	}
+
+	m.ConnectToDatabaseRecentAction()
+
+	var final []models.RecentAction
+	var err error
+	for i := 0; i < len(result.SubscribedBlogs); i++ {
+		filter = bson.M{
+			"blogEntry.id": result.SubscribedBlogs[i],
+			"timeSeconds":  bson.M{"$gte": after},
+		}
+		opts := options.Find().SetLimit(limit)
+		cursor, err := m.Collection.Find(context.TODO(), filter, opts)
+		if err != nil {
+			zap.S().Errorf("Error while finding blogs which user has subscribed")
+			return nil, nil
+		}
+		var res []models.RecentAction
+
+		if err = cursor.All(context.TODO(), &res); err != nil {
+			zap.S().Errorf("Error while decoding cursor %vth time: %v", i, err)
+		}
+
+		for i := 0; i < len(res); i++ {
+			final = append(final, res[i])
+		}
+	}
+	return final, err
 }
