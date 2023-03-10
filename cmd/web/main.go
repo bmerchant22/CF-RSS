9e6edd6ff80c23f8e2a72fe015165ada35211861
Burhanuddin Merchant <merchantburhanuddin484@gmail.com>
2023-03-11 03:21:02 +0530

    Enabled background worker and created web server in 2 different goroutines concurrently

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"github.com/bmerchant22/project/pkg/store"
+	"github.com/bmerchant22/project/pkg/web"
 	"github.com/bmerchant22/project/pkg/worker"
 	"go.uber.org/zap"
 	"log"
+	"sync"
 )
 
 func main() {
@@ -18,7 +21,24 @@ func main() {
 
 	zap.ReplaceGlobals(logger)
 
-	worker.PerformWork()
+	mongoStore := store.MongoStore{}
+
+	wg := new(sync.WaitGroup)
+	wg.Add(2)
+	go func(wg *sync.WaitGroup) {
+		defer wg.Done()
+		worker.PerformWork(&mongoStore)
+	}(wg)
+
+	func(wg *sync.WaitGroup) {
+		defer wg.Done()
+		srv := web.CreateWebServer(&mongoStore)
+		if err := srv.StartListeningForRequests(":8080"); err != nil {
+			zap.S().Errorf("Error while starting server from main : %v", err)
+		}
+	}(wg)
+
+	wg.Wait()
 
 	return
 }
