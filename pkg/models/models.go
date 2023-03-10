3307673952a4270c098403803753ad9fdc163e84
Burhanuddin Merchant <merchantburhanuddin484@gmail.com>
2023-03-11 03:10:58 +0530

    Created user model

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -15,17 +15,24 @@ type BlogEntry struct {
 }
 
 type Comment struct {
-	Id                  int    `json:"id"`
-	CreationTimeSeconds int    `json:"creationTimeSeconds"`
-	CommentatorHandle   string `json:"commentatorHandle"`
-	Locale              string `json:"locale"`
-	Text                string `json:"text"`
-	ParentCommentId     int    `json:"parentCommentId"`
-	Rating              int    `json:"rating"`
+	Id                  int    `json:"id" bson:"id"`
+	CreationTimeSeconds int    `json:"creationTimeSeconds" bson:"creationTimeSeconds"`
+	CommentatorHandle   string `json:"commentatorHandle" bson:"commentatorHandle"`
+	Locale              string `json:"locale" bson:"locale"`
+	Text                string `json:"text" bson:"text"`
+	ParentCommentId     int    `json:"parentCommentId" bson:"parentCommentId"`
+	Rating              int    `json:"rating" bson:"rating"`
 }
 
 type RecentAction struct {
-	TimeSeconds int64      `json:"timeSeconds"`
-	BlogEntry   *BlogEntry `json:"blogEntry"`
-	Comment     *Comment   `json:"comment"`
+	TimeSeconds int64      `json:"timeSeconds" bson:"timeSeconds"`
+	BlogEntry   *BlogEntry `json:"blogEntry" bson:"blogEntry"`
+	Comment     *Comment   `json:"comment" bson:"comment"`
+}
+
+type User struct {
+	Username         string `json:"username" bson:"username"`
+	Email            string `json:"email" bson:"email"`
+	CodeforcesHandle string `json:"codeforcesHandle" bson:"codeforcesHandle"'`
+	SubscribedBlogs  []int  `json:"subscribedBlogs" bson:"subscribedBlogs"`
 }
